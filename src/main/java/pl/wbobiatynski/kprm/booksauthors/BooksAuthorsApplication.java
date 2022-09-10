1e094a12e31b45adcd66e6c35ecb51d783089917
WojciechBobiatynski <59624021+WojciechBobiatynski@users.noreply.github.com>
2022-09-10 21:11:04 +0200

    Initial commit - liquibase script to create database.

---
+++ src/main/java/pl/wbobiatynski/kprm/booksauthors/BooksAuthorsApplication.java
@@ -0,0 +1,13 @@
+package pl.wbobiatynski.kprm.booksauthors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksAuthorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksAuthorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wbobiatynski/kprm/booksauthors/BooksAuthorsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wbobiatynski.kprm.booksauthors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksAuthorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
