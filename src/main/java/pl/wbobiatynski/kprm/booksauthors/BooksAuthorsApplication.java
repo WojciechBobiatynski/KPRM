163664dcb45c3c69cef03c9b726b8c423de0f239
Wojciech Bobiatyński <59624021+WojciechBobiatynski@users.noreply.github.com>
2022-09-12 15:07:55 +0200

    Update BooksAuthorsApplication.java
    Code cleaning.

--- src/main/java/pl/wbobiatynski/kprm/booksauthors/BooksAuthorsApplication.java
+++ src/main/java/pl/wbobiatynski/kprm/booksauthors/BooksAuthorsApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BooksAuthorsApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BooksAuthorsApplication.class, args);
 	}
-
 }
