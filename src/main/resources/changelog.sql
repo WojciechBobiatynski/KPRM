70ee676d26a55f6b730c011b18b154a3dc36b178
Wojciech Bobiatyński <59624021+WojciechBobiatynski@users.noreply.github.com>
2022-11-18 12:38:47 +0100

    Database initialising by new values.

--- src/main/resources/changelog.sql
+++ src/main/resources/changelog.sql
@@ -4,8 +4,7 @@ INSERT INTO Author (Id, Name, Surname) VALUES ( 1, 'Henryk', 'Sienkiewicz');
 INSERT INTO Author (Id, Name, Surname) VALUES ( 2, 'Boleslaw', 'Prus');
 INSERT INTO Author (Id, Name, Surname) VALUES ( 3, 'Adam', 'Mickiewicz');
 
-CREATE TABLE Book (Id INT, Title VARCHAR, ISBN VARCHAR, Author INT,
-    FOREIGN KEY (Author) REFERENCES Author(Id), PRIMARY KEY (Id));
+CREATE TABLE Book (Id INT, Title VARCHAR, ISBN VARCHAR, Author INT, FOREIGN KEY (Author) REFERENCES Author(Id), PRIMARY KEY (Id));
 
 INSERT INTO Book (Id, Title, ISBN, Author) VALUES ( 1, 'Ogniem i mieczem', '1234567890ABC', 1);
 INSERT INTO Book (Id, Title, ISBN, Author) VALUES ( 2, 'Potop', '1234567890DEF', 1);
